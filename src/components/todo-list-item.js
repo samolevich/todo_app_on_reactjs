9680e3ae50a8c0f25a0b8ead32f38ae597100e0c
samolevich <samolevich@gmail.com>
2020-03-30 21:21:37 +0300

    try to use state

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,29 +1,51 @@
 import React from "react";
-import './todo-list-item.css';
+import "./todo-list-item.css";
 
 export default class TodoListItem extends React.Component {
+  state = { done: false, important: false };
   onLabelClick = () => {
-      console.log(`Done: ${this.props.label}`)
+    this.setState((state) => {
+      return {
+        done: !state.done
+      }
+    });
+    console.log(`Done: ${this.props.label}`);
+  };
+  onMarkImportant = () => {
+    this.setState((state) => {
+      return {
+        important: !state.important
+      }
+    });
+  };
+  render() {
+    const { label } = this.props;
+    const { done, important } = this.state;
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += " done";
     }
-  render () {
-    const {label, important = false} = this.props;
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
+    if (important) {
+      classNames += " important";
     }
     return (
       <span>
-        <span style={style} className='todo-list-item'
-        onClick={this.onLabelClick}>
+        <span className={classNames} onClick={this.onLabelClick}>
           {label}
         </span>
-        <button type='button' className='btn btn-outline-success btn-sm'>
-          <i className='fa fa-exclamation' />
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+        >
+          <i className="fa fa-exclamation" />
         </button>
-        <button type='button' className='btn btn-outline-danger btn-sm'>
-          <i className='fa fa-trash-o' />
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+        >
+          <i className="fa fa-trash-o" />
         </button>
       </span>
-    )
+    );
   }
 }
