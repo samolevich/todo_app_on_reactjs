53b6d4aadf4bbcad201ef3f68e97027ed954619f
samolevich <samolevich@gmail.com>
2020-03-30 15:46:49 +0300

    insert some information for visualisation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Let's start</h1>
+const element = (
+  <div>
+    <h1>To Do List</h1>
+    <input placeholder='search'/>
+    <ul>
+      <li>Learn React</li>
+      <li>Build Perfect App</li>
+    </ul>
+  </div>
+)
 
 ReactDOM.render(element, document.getElementById('root'));
