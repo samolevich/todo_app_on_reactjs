7b02ba4b541643ed44c82d2bfc2dbcfd5483b30e
samolevich <samolevich@gmail.com>
2020-03-30 17:42:05 +0300

    use map for array of data for list of points

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,38 +2,20 @@ import React from "react";
 import TodoListItem from "./todo-list-item";
 
 const TodoList = ({todos}) => {
+  const points = todos.map(point => {
+    return (
+      <li>
+        <TodoListItem
+          label={point.label}
+          important={point.important}
+        />
+      </li>
+    )
+  })
+
   return (
     <ul>
-      <li>
-        <TodoListItem
-          label={todos[0].label}
-          important={todos[0].important}
-        />
-      </li>
-      <li>
-        <TodoListItem
-          label={todos[1].label}
-          important={todos[1].important}
-        />
-      </li>
-      <li>
-        <TodoListItem
-          label={todos[2].label}
-          important={todos[2].important}
-        />
-      </li>
-      <li>
-        <TodoListItem
-          label={todos[3].label}
-          important={todos[3].important}
-        />
-      </li>
-      <li>
-        <TodoListItem
-          label={todos[4].label}
-          important={todos[4].important}
-        />
-      </li>
+      {points}
     </ul>
   );
 };
