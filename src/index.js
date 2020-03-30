fd9f412c82df09eacf23ba08ef122e86de72f47e
samolevich <samolevich@gmail.com>
2020-03-30 16:21:48 +0300

    trying some features

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const TodoList = () => {
+  const points = ["Learn React", "Build Perfect App"];
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Build Perfect App</li>
+      <li>{points[0]}</li>
+      <li>{points[1]}</li>
     </ul>
   );
 };
@@ -18,12 +19,19 @@ const SearchPanel = () => {
   return <input placeholder="search" />;
 };
 
+const Today = () => {
+  return (String(new Date()))
+}
+
 const App = () => {
+  const login = <span>Login please</span>
   return (
     <div>
+      <Today />
       <AppHeader />
       <SearchPanel />
       <TodoList />
+      {login}
     </div>
   );
 };
