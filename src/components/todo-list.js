329a556c8a5c5433c84b58d037ea8b32f0b0d2df
samolevich <samolevich@gmail.com>
2020-03-30 17:46:13 +0300

    use spread for all object properties

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -5,10 +5,7 @@ const TodoList = ({todos}) => {
   const points = todos.map(point => {
     return (
       <li>
-        <TodoListItem
-          label={point.label}
-          important={point.important}
-        />
+        <TodoListItem {...point} />
       </li>
     )
   })
