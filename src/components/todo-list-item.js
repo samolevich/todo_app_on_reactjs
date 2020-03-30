c5bc73227f36e9f1c48ff3b8189f5ee435033c92
samolevich <samolevich@gmail.com>
2020-03-30 19:37:25 +0300

    rewrite ItemStatusFilter and TodoListItem on classes

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,23 +1,26 @@
 import React from "react";
 import './todo-list-item.css';
 
-const TodoListItem = ({label, important = false}) => {
-  const style = {
-    fontWeight: important ? 'bold' : 'normal'
-  }
-  return (
-    <span>
-      <span style={style} className='todo-list-item'>
-        {label}
-      </span>
-      <button type='button' className='btn btn-outline-success btn-sm'>
-        <i className='fa fa-exclamation' />
-      </button>
-      <button type='button' className='btn btn-outline-danger btn-sm'>
-        <i className='fa fa-trash-o' />
-      </button>
-    </span>
-  )
-};
+export default class TodoListItem extends React.Component {
 
-export default TodoListItem;
+  render () {
+    const {label, important = false} = this.props;
+    const style = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    }
+    return (
+      <span>
+        <span style={style} className='todo-list-item'>
+          {label}
+        </span>
+        <button type='button' className='btn btn-outline-success btn-sm'>
+          <i className='fa fa-exclamation' />
+        </button>
+        <button type='button' className='btn btn-outline-danger btn-sm'>
+          <i className='fa fa-trash-o' />
+        </button>
+      </span>
+    )
+  }
+}
